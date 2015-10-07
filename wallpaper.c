7df825bab8918d9942131efd4aa20f29901b9cb9
Sindre Sorhus <sindresorhus@gmail.com>
2015-10-07 18:16:20 +0700

    unicode support  - fixes #1
    thanks to @Qix-

--- wallpaper.c
+++ wallpaper.c
@@ -1,34 +1,35 @@
 #include <stdio.h>
 #include <windows.h>
 
-int main(int argc, char **argv) {
+int wmain(int argc, wchar_t **argv) {
 	if (argc > 1) {
-		if (strcmp(argv[1], "--version") == 0) {
+		if (wcscmp(argv[1], L"--version") == 0) {
 			puts("1.0.0");
 			return 0;
 		}
 
-		if (strcmp(argv[1], "--help") == 0) {
+		if (wcscmp(argv[1], L"--help") == 0) {
 			puts("\n  Get or set the desktop wallpaper\n\n  Usage: wallpaper [file]\n\n  Created by Sindre Sorhus");
 			return 0;
 		}
 
-		char fullPath[MAX_PATH];
+		wchar_t fullPath[MAX_PATH];
 
-		if (!_fullpath(fullPath, argv[1], MAX_PATH)) {
+		if (!_wfullpath(fullPath, argv[1], MAX_PATH)) {
 			fputs("Invalid path", stderr);
 			return 1;
 		}
 
-		if (!SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, fullPath, SPIF_SENDCHANGE)) {
+		if (!SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, fullPath, SPIF_SENDCHANGE)) {
 			fputs("Failed to set the desktop wallpaper", stderr);
 			return 1;
 		}
 	} else {
-		char imagePath[MAX_PATH];
+		wchar_t imagePath[MAX_PATH];
 
-		if (SystemParametersInfo(SPI_GETDESKWALLPAPER, sizeof(imagePath) - 1, imagePath, 0)) {
-			puts(imagePath);
+		if (SystemParametersInfoW(SPI_GETDESKWALLPAPER, MAX_PATH, imagePath, 0)) {
+			DWORD written;
+			WriteConsoleW(GetStdHandle(STD_OUTPUT_HANDLE), &imagePath[0], wcslen(&imagePath[0]), &written, 0);
 		} else {
 			fputs("Failed to get the desktop wallpaper", stderr);
 			return 1;
