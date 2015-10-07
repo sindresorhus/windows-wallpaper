bfaecb8a003da2f9da8802903d265d24f9193718
Sindre Sorhus <sindresorhus@gmail.com>
2015-10-07 17:21:33 +0700

    use `return` instead of `exit()` - fixes #2

--- wallpaper.c
+++ wallpaper.c
@@ -5,24 +5,24 @@ int main(int argc, char **argv) {
 	if (argc > 1) {
 		if (strcmp(argv[1], "--version") == 0) {
 			puts("1.0.0");
-			exit(0);
+			return 0;
 		}
 
 		if (strcmp(argv[1], "--help") == 0) {
 			puts("\n  Get or set the desktop wallpaper\n\n  Usage: wallpaper [file]\n\n  Created by Sindre Sorhus");
-			exit(0);
+			return 0;
 		}
 
 		char fullPath[MAX_PATH];
 
 		if (!_fullpath(fullPath, argv[1], MAX_PATH)) {
 			fputs("Invalid path", stderr);
-			exit(1);
+			return 1;
 		}
 
 		if (!SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, fullPath, SPIF_SENDCHANGE)) {
 			fputs("Failed to set the desktop wallpaper", stderr);
-			exit(1);
+			return 1;
 		}
 	} else {
 		char imagePath[MAX_PATH];
@@ -31,7 +31,7 @@ int main(int argc, char **argv) {
 			puts(imagePath);
 		} else {
 			fputs("Failed to get the desktop wallpaper", stderr);
-			exit(1);
+			return 1;
 		}
 	}
 
